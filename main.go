76bd7a2ad6270e5bf22db5d65fd23229b847af0d
Roy Zwambag <royzwambag@gmail.com>
2020-08-15 11:29:37 +0200

    log some data

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/sfreiberg/gotwilio"
 )
 
 func main() {
+	// Setup vars
 	accountSid := os.Getenv("PLUGIN_ACCOUNT_SID")
 	authToken := os.Getenv("PLUGIN_AUTH_TOKEN")
 	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
@@ -17,9 +19,20 @@ func main() {
 
 	messageType := os.Getenv("PLUGIN_TYPE")
 
+	var resp *gotwilio.SmsResponse
+	var exception *gotwilio.Exception
+	var err error
+
+	// Actual sending of the message
 	if messageType == "whatsapp" {
-		twilio.SendWhatsApp(from, to, body, "", "")
+		resp, exception, err = twilio.SendWhatsApp(from, to, body, "", "")
 	} else {
-		twilio.SendSMS(from, to, body, "", "")
+		resp, exception, err = twilio.SendSMS(from, to, body, "", "")
 	}
+
+	if err != nil || exception != nil {
+		fmt.Println(err, exception)
+		os.Exit(1)
+	}
+	fmt.Println("Message send:", resp)
 }
